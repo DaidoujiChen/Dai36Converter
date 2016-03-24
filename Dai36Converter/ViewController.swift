943a57d7dfffe4d504d7964bcb5d581e79c6db03
DaidoujiChen <daidoujichen@gmail.com>
2016-03-24 14:56:58 +0800

    接上 tinyurl api

--- Dai36Converter/ViewController.swift
+++ Dai36Converter/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: NSViewController {
 
     @IBOutlet weak var inputTextField: NSTextField!
     @IBOutlet weak var outputTextField: NSTextField!
+    @IBOutlet weak var resultTextField: NSTextField!
 
     @IBAction func onPressConvertAction(sender: AnyObject) {
         let splitInput = self.inputTextField.stringValue.componentsSeparatedByString("-")
@@ -20,6 +21,21 @@ class ViewController: NSViewController {
             result <+= ThirtySix(by: input)
         }
         self.outputTextField.stringValue = result.value
+        
+        if let safeURL = NSURL(string: "http://tinyurl.com/api-create.php?url=moemoeder://?unlock=\(result.value)") {
+            NSURLSession.sharedSession().dataTaskWithURL(safeURL, completionHandler: { [weak self] (data, response, error) -> Void in
+                guard let safeSelf = self else {
+                    return
+                }
+                
+                if let safeData = data, let safeResult = String(data: safeData, encoding: NSUTF8StringEncoding) where error == nil {
+                    safeSelf.resultTextField.stringValue = safeResult
+                }
+                else {
+                    safeSelf.resultTextField.stringValue = "Some Thing Wrong"
+                }
+            }).resume()
+        }
     }
     
 }
